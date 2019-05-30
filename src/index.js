88aaadc6c2f341964ac39db1117b5015491d3bf2
Laura Badgley <l.badgleyy@gmail.com>
2019-05-30 13:29:17 -0700

    commit with everything

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import { createStore, applyMiddleware } from 'redux';
+
+const logger = store => next => action => {
+  console.log('Before Reduxer', store.getState());
+  next(action);
+  console.log('After reducer', store.getState());
+};
+
+const logger2 = store => next => action => {
+  console.log('i am another middleware!', action);
+  next(action);
+};
+
+function reducer(state = {}, action) {
+  switch(action.type) {
+    case 'HI':
+      return 'hi';
+    default: 
+      return state;
+  }
+}
+const store = createStore(
+  reducer,
+  applyMiddleware(logger, logger2)
+);
+
+store.dispatch({
+  type: 'HI'
+});
